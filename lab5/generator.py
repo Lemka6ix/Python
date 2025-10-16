82942639b27300171f968170ca08af8ee5fa9b50
Lemka6ix <klider.fire@mail.ru>
2025-10-16 21:23:17 +0300

    frog

--- lab5/generator.py
+++ lab5/generator.py
@@ -1,15 +1,19 @@
 n = int(input())
 matrix = [list(map(int, input().split())) for i in range(n)]
 
+if n % 2 ==0:
+    print('generator cannot bypass an even matrix')
+    exit()
+
 def spiral_generator(matrix):
     n = len(matrix)    # размер
     x, y = n // 2, n // 2  # центр
     dx, dy = 0, 1   # задаем направление по час. вправо
-    step = 1     
-    count = 1
+    step = 1     # Длина первого шага
+    count = 1    # посетили центр
     yield matrix[x][y], x, y   # Возвращаем центральный элемент
     while count < n * n:
-        for _ in range(step):
+        for i in range(step):
             x += dx
             y += dy
             if 0 <= x < n and 0 <= y < n:
