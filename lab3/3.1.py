fcb41216b4ebfa5859d88dcabab48dd1bc113224
aoykitz <defember1982@mail.ru>
2025-10-17 15:34:09 +0500

    3

--- lab3/3.1.py
+++ lab3/3.1.py
@@ -2,12 +2,16 @@ def plen(nums, k):
     if k == 0:
         return [[]]
     
+    if k > len(nums):
+        return []
     result = []
-    for i, num in enumerate(nums):
-        rest = nums[:i] + nums[i + 1:]
-        for perm in plen(rest, k - 1):
-            result.append([num] + perm)
-    
+    for i in range(len(nums)):
+        current_nums = nums[i]
+        remain_nums = nums[:i] + nums[i+1:]
+        smal_perm = plen(remain_nums, k-1)
+
+        for i in smal_perm:
+            result.append([current_nums] + i)
     return result
 
 print(plen([1, 2, 3], 2))
